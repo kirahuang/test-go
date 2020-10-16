f55d18a8fe7ade2b005a2cd958bef919dbe1981b
huang <huangyinhai@bestpay.com.cn>
2020-10-16 15:45:00 +0800

    http 1

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	greetings "github.com/kirahuang/test-go/src/test"
-
+	httpUtil "github.com/kirahuang/test-go/src/http"
 )
 
 func main() {
 	// Get a greeting message and print it.
 	message := greetings.Hello("Gladys")
 	fmt.Println(message)
+
+
+		bytes:=httpRequest("https://www.baidu.com")
+		str := String(bytes)
+		//bytes[0] = 'i'//注意这一行，bytes在这里修改了数据，但是str打印出来的依然没变化，
+		fmt.Println(str)
 }
